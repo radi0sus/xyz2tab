1634e6cc12e10be2ee2552d12c44f195b39d9fe5
Sebastian Dechert <p212121@googlemail.com>
2021-11-21 09:55:05 +0100

    Update xyz2tab.py

--- xyz2tab.py
+++ xyz2tab.py
@@ -10,8 +10,7 @@ import pandas as pd                                         #pandas tables
 import numpy as np                                          #for calculations
 from scipy.spatial.distance import pdist, squareform	    #for the calculations of the distance matrix
 from tabulate import tabulate                               #nice table output
-import gemmi	                                            #covalence radii, molecular weight; can be replaced by own or other
-                                                            #covalent radii and mol. weight lists
+#import gemmi	                                            #covalence radii, molecular weight
 
 #for windows console
 sys.stdout.reconfigure(encoding='utf-8')  
@@ -19,7 +18,136 @@ sys.stdout.reconfigure(encoding='utf-8')
 #pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 #+x % to covalence radius
-radii_ext = 11.15 / 100 
+radii_ext = 8 / 100 
+
+#covalent radii from Alvarez (2008)
+#DOI: 10.1039/b801115j
+covalent_radii = {
+	'H': 0.31,
+	'He': 0.28,
+	'Li': 1.28,
+	'Be': 0.96,
+	'B': 0.84,
+	'C': 0.76,
+	'N': 0.71,
+	'O': 0.66,
+	'F': 0.57,
+	'Ne': 0.58,
+	'Na': 1.66,
+	'Mg': 1.41,
+	'Al': 1.21,
+	'Si': 1.11,
+	'P': 1.07,
+	'S': 1.05,
+	'Cl': 1.02,
+	'Ar': 1.06,
+	'K': 2.03,
+	'Ca': 1.76,
+	'Sc': 1.70,
+	'Ti': 1.60,
+	'V': 1.53,
+	'Cr': 1.39,
+	'Mn': 1.61,
+	'Fe': 1.52,
+	'Co': 1.50,
+	'Ni': 1.24,
+	'Cu': 1.32,
+	'Zn': 1.22,
+	'Ga': 1.22,
+	'Ge': 1.20,
+	'As': 1.19,
+	'Se': 1.20,
+	'Br': 1.20,
+	'Kr': 1.16,
+	'Rb': 2.20,
+	'Sr': 1.95,
+	'Y': 1.90,
+	'Zr': 1.75,
+	'Nb': 1.64,
+	'Mo': 1.54,
+	'Tc': 1.47,
+	'Ru': 1.46,
+	'Rh': 1.42,
+	'Pd': 1.39,
+	'Ag': 1.45,
+	'Cd': 1.44,
+	'In': 1.42,
+	'Sn': 1.39,
+	'Sb': 1.39,
+	'Te': 1.38,
+	'I': 1.39,
+	'Xe': 1.40,
+	'Cs': 2.44,
+	'Ba': 2.15,
+	'La': 2.07,
+	'Ce': 2.04,
+	'Pr': 2.03,
+	'Nd': 2.01,
+	'Pm': 1.99,
+	'Sm': 1.98,
+	'Eu': 1.98,
+	'Gd': 1.96,
+	'Tb': 1.94,
+	'Dy': 1.92,
+	'Ho': 1.92,
+	'Er': 1.89,
+	'Tm': 1.90,
+	'Yb': 1.87,
+	'Lu': 1.87,
+	'Hf': 1.75,
+	'Ta': 1.70,
+	'W': 1.62,
+	'Re': 1.51,
+	'Os': 1.44,
+	'Ir': 1.41,
+	'Pt': 1.36,
+	'Au': 1.36,
+	'Hg': 1.32,
+	'Tl': 1.45,
+	'Pb': 1.46,
+	'Bi': 1.48,
+	'Po': 1.40,
+	'At': 1.50,
+	'Rn': 1.50,
+	'Fr': 2.60,
+	'Ra': 2.21,
+	'Ac': 2.15,
+	'Th': 2.06,
+	'Pa': 2.00,
+	'U': 1.96,
+	'Np': 1.90,
+	'Pu': 1.87,
+	'Am': 1.80,
+	'Cm': 1.69,
+}
+
+#atomic weights
+atomic_weights = {
+	'H' : 1.008,'He' : 4.003, 'Li' : 6.941, 'Be' : 9.012,
+	'B' : 10.811, 'C' : 12.011, 'N' : 14.007, 'O' : 15.999,
+	'F' : 18.998, 'Ne' : 20.180, 'Na' : 22.990, 'Mg' : 24.305,
+	'Al' : 26.982, 'Si' : 28.086, 'P' : 30.974, 'S' : 32.066,
+	'Cl' : 35.453, 'Ar' : 39.948, 'K' : 39.098, 'Ca' : 40.078,
+	'Sc' : 44.956, 'Ti' : 47.867, 'V' : 50.942, 'Cr' : 51.996,
+	'Mn' : 54.938, 'Fe' : 55.845, 'Co' : 58.933, 'Ni' : 58.693,
+	'Cu' : 63.546, 'Zn' : 65.38, 'Ga' : 69.723, 'Ge' : 72.631,
+	'As' : 74.922, 'Se' : 78.971, 'Br' : 79.904, 'Kr' : 84.798,
+	'Rb' : 84.468, 'Sr' : 87.62, 'Y' : 88.906, 'Zr' : 91.224,
+	'Nb' : 92.906, 'Mo' : 95.95, 'Tc' : 98.907, 'Ru' : 101.07,
+	'Rh' : 102.906, 'Pd' : 106.42, 'Ag' : 107.868, 'Cd' : 112.414,
+	'In' : 114.818, 'Sn' : 118.711, 'Sb' : 121.760, 'Te' : 126.7,
+	'I' : 126.904, 'Xe' : 131.294, 'Cs' : 132.905, 'Ba' : 137.328,
+	'La' : 138.905, 'Ce' : 140.116, 'Pr' : 140.908, 'Nd' : 144.243,
+	'Pm' : 144.913, 'Sm' : 150.36, 'Eu' : 151.964, 'Gd' : 157.25,
+	'Tb' : 158.925, 'Dy': 162.500, 'Ho' : 164.930, 'Er' : 167.259,
+	'Tm' : 168.934, 'Yb' : 173.055, 'Lu' : 174.967, 'Hf' : 178.49,
+	'Ta' : 180.948, 'W' : 183.84, 'Re' : 186.207, 'Os' : 190.23,
+	'Ir' : 192.217, 'Pt' : 195.085, 'Au' : 196.967, 'Hg' : 200.592,
+	'Tl' : 204.383, 'Pb' : 207.2, 'Bi' : 208.980, 'Po' : 208.982,
+	'At' : 209.987, 'Rn' : 222.081, 'Fr' : 223.020, 'Ra' : 226.025,
+	'Ac' : 227.028, 'Th' : 232.038, 'Pa' : 231.036, 'U' : 238.029,
+	'Np' : 237, 'Pu' : 244, 'Am' : 243, 'Cm' : 247
+}
 
 #dict for numbers to subscript numbers
 utf_sub_dict = {
@@ -117,9 +245,9 @@ parser.add_argument('-ic','--includeCon',
 
 #include contacts
 parser.add_argument('-r','--radius',
-	default=11.15,
+	default=8,
 	type=float,
-	help='enlarge atomic radii by x %%, e.g. -r 15.2, default is 11.15 %%')
+	help='enlarge atomic radii by x %%, e.g. -r 15.2, default is 8 %%')
 
 #verbose
 parser.add_argument('-v','--verbose',
@@ -149,10 +277,14 @@ radii_ext = args.radius / 100
 xyz_df['atom1_idx'] = ["{}{}".format(atm, idx) for atm, idx in zip(xyz_df.element, xyz_df.index.array)]
 #atom 1 is atom 2
 xyz_df['atom2_idx'] = xyz_df['atom1_idx']
-#atomic weight
-xyz_df['weight'] = xyz_df['element'].apply(lambda x: gemmi.Element(x).weight)
-#covalent radius
-xyz_df['cov_radius'] = xyz_df['element'].apply(lambda x: gemmi.Element(x).covalent_r)
+#atomic weight gemmi
+#xyz_df['weight'] = xyz_df['element'].apply(lambda x: gemmi.Element(x).weight)
+#atomic weight dict
+xyz_df['weight'] = xyz_df['element'].apply(lambda x: atomic_weights[x])
+#covalent radius gemmi
+#xyz_df['cov_radius'] = xyz_df['element'].apply(lambda x: gemmi.Element(x).covalent_r)
+#covalent radius dict
+xyz_df['cov_radius'] = xyz_df['element'].apply(lambda x: covalent_radii[x])
 #reorder data frame
 xyz_df=xyz_df[['atom1_idx','atom2_idx','element','x','y','z','weight','cov_radius']]
 #formula weight from sum of atomic weights
@@ -173,9 +305,15 @@ sum_formula_list=list("{}{}".format(element, number) for element, number in zip(
 #drop the utf8 subscript number column
 info_df.drop(columns=['utf_num'], inplace=True)
 #calculate mass fraction of each element
-info_df['atom%']=info_df.apply(lambda x: gemmi.Element(x.element).weight*x.atom1_idx/fw*100, axis=1)
+#gemmi
+#info_df['atom%']=info_df.apply(lambda x: gemmi.Element(x.element).weight*x.atom1_idx/fw*100, axis=1)
+#dict
+info_df['atom%']=info_df.apply(lambda x: atomic_weights[x.element]*x.atom1_idx/fw*100, axis=1)
 #atomic radii from the gemmi table
-info_df['radii']=info_df.apply(lambda x: gemmi.Element(x.element).covalent_r, axis=1)
+#gemmi
+#info_df['radii']=info_df.apply(lambda x: gemmi.Element(x.element).covalent_r, axis=1)
+#dict
+info_df['radii']=info_df.apply(lambda x: covalent_radii[x.element], axis=1)
 #atomic radii + x% used for calculation of bonds
 info_df['radii_plus']=info_df.apply(lambda x: x.radii + x.radii*radii_ext, axis=1)
 
@@ -198,7 +336,6 @@ print(tabulate(info_df,
 			tablefmt='github',
 			floatfmt=(".2f"),
 			showindex=False))
-
 #calculate the full distance matrix & put to square form, e.g.:
 #
 #   C0  C1  C2
